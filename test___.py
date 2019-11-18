bed9fca75ec76636381bc50e3b186ee462195430
Ali Babolhavaeji <40876922+Alibhji@users.noreply.github.com>
2019-11-18 13:07:58 -0500

    3D Plot is added to gui_tools.py fix bugs

--- test___.py
+++ test___.py
@@ -94,6 +94,9 @@
 # plt.show()
 
 
+
+
+
 import numpy as np
 import matplotlib.pylab as plt
 
@@ -136,4 +139,55 @@ fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot_wireframe(X, Y, Z, color='black')
 ax.set_title('wireframe');
+plt.show()
+
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.linspace(-50,50,100)
+y = np.arange(25)
+X,Y = np.meshgrid(x,y)
+Z = np.zeros((len(y),len(x)))
+
+for i in range(len(y)):
+    damp = (i/float(len(y)))**2
+    Z[i] = 5*damp*(1 - np.sqrt(np.abs(x/50)))
+    Z[i] += np.random.uniform(0,.1,len(Z[i]))
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1000, color='w', shade=False, lw=.5)
+
+ax.set_zlim(0, 5)
+ax.set_xlim(-51, 51)
+ax.set_zlabel("Intensity")
+ax.view_init(20,-120)
+plt.show()
+
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.linspace(-50,50,100)
+y = np.arange(25)
+X,Y = np.meshgrid(x,y)
+Z = np.zeros((len(y),len(x)))
+
+for i in range(len(y)):
+    damp = (i/float(len(y)))**2
+    Z[i] = 5*damp*(1 - np.sqrt(np.abs(x/50)))
+    Z[i] += np.random.uniform(0,.1,len(Z[i]))
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1000, color='w', shade=False, lw=.5)
+
+
+ax.set_zlim(0, 5)
+ax.set_xlim(-51, 51)
+ax.set_zlabel("Intensity")
+ax.view_init(20,-120)
 plt.show()
